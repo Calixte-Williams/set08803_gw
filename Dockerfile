2965098ab52026a04f72fab57d6b3d2822b3d0aa
Calixte Williams <calixte.williams@gmail.com>
2024-03-05 14:02:33 -0400

    1. Add Country Class for reports 2. Change database connection password 3. Update App to allow handling of methods using calls in the main section of the App file. 4. Create first use-case diagram for "World Countries Report" 5. Update version number v0.1.0.3

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:latest
-COPY ./target/set08803_gw-0.1.0.2-jar-with-dependencies.jar /tmp
+COPY ./target/set08803_gw-0.1.0.3-jar-with-dependencies.jar /tmp
 WORKDIR /tmp
-ENTRYPOINT ["java", "-jar", "set08803_gw-0.1.0.2-jar-with-dependencies.jar"]
+ENTRYPOINT ["java", "-jar", "set08803_gw-0.1.0.3-jar-with-dependencies.jar"]
--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -6,46 +6,66 @@ public class App
 {
     public static void main(String[] args)
     {
-        try
+        // Create new Application
+        App a = new App();
+
+        // Connect to database
+        a.connect();
+
+        // Disconnect from database
+        a.disconnect();
+    }
+
+    /**
+     * Connection to MySQL database.
+     */
+    private Connection con = null;
+
+    /**
+     * Connect to the MySQL database.
+     */
+
+        public void connect()
         {
-            // Load Database driver
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        }
-        catch (ClassNotFoundException e)
-        {
-            System.out.println("Could not load SQL driver");
-            System.exit(-1);
+            try {
+                // Load Database driver
+                Class.forName("com.mysql.cj.jdbc.Driver");
+            } catch (ClassNotFoundException e) {
+                System.out.println("Could not load SQL driver");
+                System.exit(-1);
+            }
+
+            // Connection to the database
+            Connection con = null;
+            int retries = 100;
+            for (int i = 0; i < retries; ++i) {
+                System.out.println("Connecting to database...");
+                try {
+                    // Wait a bit for db to start
+                    Thread.sleep(30000);
+                    // Connect to database
+                    con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "w0rldDBp@ss");
+                    System.out.println("Successfully connected");
+                    // Wait a bit
+                    Thread.sleep(10000);
+                    // Exit for loop
+                    break;
+                } catch (SQLException sqle) {
+                    System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+                    System.out.println(sqle.getMessage());
+                } catch (InterruptedException ie) {
+                    System.out.println("Thread interrupted? Should not happen.");
+                }
+            }
         }
 
-        // Connection to the database
-        Connection con = null;
-        int retries = 100;
-        for (int i = 0; i < retries; ++i)
-        {
-            System.out.println("Connecting to database...");
-            try
-            {
-                // Wait a bit for db to start
-                Thread.sleep(30000);
-                // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
-                System.out.println("Successfully connected");
-                // Wait a bit
-                Thread.sleep(10000);
-                // Exit for loop
-                break;
-            }
-            catch (SQLException sqle)
-            {
-                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
-                System.out.println(sqle.getMessage());
-            }
-            catch (InterruptedException ie)
-            {
-                System.out.println("Thread interrupted? Should not happen.");
-            }
-        }
+       //public Country getCountriesByPopulation(){}
 
+    /**
+     * Disconnect from the MySQL database.
+     */
+    public void disconnect()
+    {
         if (con != null)
         {
             try
