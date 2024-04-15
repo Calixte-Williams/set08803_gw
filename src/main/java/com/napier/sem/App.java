b987ffc4f50969a3a65e9967ea73aabd93f6aad0
Jermon1 <40615063@live.napier.ac.uk>
2024-04-15 17:47:21 -0400

    created a method to generate report 9

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -36,6 +36,9 @@ public class App {
         //Method to display cities in the world by population in a continent
         a.getCitiesByPopinAContinent("Europe");
 
+        //Method to display cities in the world by population in a region
+        a.getCitiesbyPopinARegion("Caribbean");
+
         // Disconnect from database
         a.disconnect();
 
@@ -474,5 +477,52 @@ public class App {
 
 
     }
-}
+
+    public ArrayList<City> getCitiesbyPopinARegion(String Region) {
+        ArrayList<City> cityList = new ArrayList<>();
+        try {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT city.Name, country.Name, city.District, city.Population "
+                            + "FROM world.city "
+                            + "JOIN world.country on city.CountryCode = country.Code "
+                            + "WHERE country.region = '" + Region + "'"
+                            + "ORDER BY Population desc";
+
+
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Process the result set and add to the list
+            while (rset.next()) {
+                City city = new City();
+                city.name = rset.getString("city.name");
+                city.country_name = rset.getString("country.name");
+                city.district = rset.getString("city.district");
+                city.population = rset.getInt("city.population");
+                cityList.add(city);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get country details");
+            return null;
+        }
+
+        // Print header
+        System.out.println(String.format("%-15s %-30s %-15s %-30s", "Name", "Country", "District", "Population"));
+        // Print each country's details
+        for (City city : cityList) {
+            String country_string =
+                    String.format("%-15s %-30s %-15s %-30s",
+                            city.name, city.country_name, city.district, city.population);
+            System.out.println(country_string);
+        }
+        return cityList;
+
+
+         }
+    }
+
 
