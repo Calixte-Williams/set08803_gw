d56bc0e33ce2bbb8211e5873a6dd8dd97a5f0f6d
Jermon1 <40615063@live.napier.ac.uk>
2024-04-17 17:42:10 -0400

    added method for report 32 and added language java class for report

---
+++ src/main/java/com/napier/sem/Language.java
@@ -0,0 +1,17 @@
+package com.napier.sem;
+
+public class Language {
+
+
+    /* Language */
+    public String language_name;
+
+    /* Total No. of Speakers */
+    public long total_speakers;
+
+
+    /* World Percent */
+    public long world_percentage;
+
+
+}
