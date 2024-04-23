adad7d6e7bb7793a55c4be10cae1e4061a852993
Calixte Williams <calixte.williams@gmail.com>
2024-04-23 16:52:45 -0400

    Add Validation Check to all Methods and Change int input to Integer inputs for all Methods

--- src/test/java/com/napier/sem/AppTest.java
+++ src/test/java/com/napier/sem/AppTest.java
@@ -1,14 +1,8 @@
 package com.napier.sem;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import org.junit.jupiter.api.TestInstance;
-
-import java.sql.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -26,8 +20,6 @@ public class AppTest {
 
     @Test
     void getCountriesByPopulationTest() {
-        ArrayList<Country> expectedCountries = new ArrayList<>();
-
         // Act
         ArrayList<Country> actualCountries = app.getCountriesByPopulation();
 
@@ -39,29 +31,12 @@ public class AppTest {
 
 
     @Test
-    void getTopNCountriesInWorldByPopThrowsExceptionTest() {
+    void getTopNCountriesInWorldByPopTestExpectThree() {
         // Act
         ArrayList<Country> actualCountries = app.getTopNCountriesInWorldByPop(3);
 
         // Assert
         //Expects the total number of Countries from the World DB (232) and fails if incorrect.
         assertEquals(3, actualCountries.size());
-
-
-        /*
-        Exception thrown = Assertions.assertThrows(Exception.class, () -> {
-            //Code under test
-            app.getTopNCountriesInWorldByPop(3);
-        });
-
-        Assertions.assertEquals("Fail! Invalid Number input for Query", thrown.getMessage());
-    }
-
-        /*
-        assertThrows(Exception.class, () -> {
-            app.getTopNCountriesInWorldByPop(3);
-        });
-
-         */
     }
 }
