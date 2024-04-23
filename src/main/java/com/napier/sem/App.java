d7632d667c57ebdf0f97cee7ac3d8d0216f00d4a
Calixte Williams <calixte.williams@gmail.com>
2024-04-23 11:24:53 -0400

    Increase retries for connection to allow github actions to connect to DB

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -139,7 +139,7 @@ public class App {
         }
 
         // Connection to the database
-        int retries = 10;
+        int retries = 100;
         boolean shouldWait = false;
         for (int i = 0; i < retries; ++i) {
             System.out.println("Connecting to database...");
